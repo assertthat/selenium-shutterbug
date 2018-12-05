2f98faad377398bb1c3e73bf963fedf449601662
Glib Briia <glib.briia@gmail.com>
2018-12-05 11:26:24 +0000

    Add secrets

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-     openssl aes-256-cbc -K $encrypted_ff4bd488efde_key -iv $encrypted_ff4bd488efde_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+      openssl aes-256-cbc -K $encrypted_8e13cbe74349_key -iv $encrypted_8e13cbe74349_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 fi
