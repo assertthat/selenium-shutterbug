ccdddae6c81d34d55975624c8fce1c2787bc5d46
Briia <glib.briia@gmail.com>
2019-05-29 15:12:49 +0100

    Fix signing

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
       openssl aes-256-cbc -K $encrypted_8e13cbe74349_key -iv $encrypted_8e13cbe74349_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+	  gpg --fast-import cd/codesigning.asc
 fi
