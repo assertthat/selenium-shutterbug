f87595dee67e4e54dd88134f7ba03b9b2ddb15a3
Briia <glib.briia@gmail.com>
2019-05-29 15:00:01 +0100

    Switch to maven

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    ./gradlew uploadArchives -Psigning.keyId=$GPG_KEY_NAME -Psigning.password=$GPG_PASSPHRASE
+    mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
 fi
