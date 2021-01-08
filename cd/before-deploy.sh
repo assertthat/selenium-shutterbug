a3f545ed791552e1cda95ee14b3b0ddc5cc56dfe
Glib Briia <glib.briia@assertthat.com>
2021-01-08 12:45:58 +0000

    Update before-deploy.sh

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ -n "$TRAVIS_TAG" ]; then
+if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] || [ -n "$TRAVIS_TAG" ]; then
       openssl aes-256-cbc -K $encrypted_8e13cbe74349_key -iv $encrypted_8e13cbe74349_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 	  gpg --fast-import cd/codesigning.asc
 fi
